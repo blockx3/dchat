368e2f5e31588030ff511de3c58531af2b7ce74c
Thinley <thinleylama44@gmail.com>
2024-10-08 23:38:04 +0530

    FEAT: Implement carousel home page

--- web/src/app/components/Home/Description.tsx
+++ web/src/app/components/Home/Description.tsx
@@ -1,12 +1,14 @@
 export default function Description() {
   return (
-    <div className="text-center text-white">
-      <div className="text-4xl font-bold pb-4">
-        Our <span className="text-[#27219C]">LP Army</span>
-      </div>
-      <div>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry standard dummy text ever
+    <div className="flex justify-center w-full text-white">
+      <div className=" text-center">
+        <div className="text-4xl md:text-6xl font-bold pb-4">
+          Our <span className="text-[#27219C]">LP Army</span>
+        </div>
+        <div className="md:text-xl w-2/3 mx-auto">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry standard dummy text ever
+        </div>
       </div>
     </div>
   );
--- web/src/app/components/Home/SliderPic.tsx
+++ web/src/app/components/Home/SliderPic.tsx
@@ -1,7 +1,44 @@
+'use client'
+
+import * as React from "react";
+
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay"
+
 export default function SliderPic() {
+  const message = "Welcome_To_dchat";
+  const characters = Array.from(message); // Convert string to an array of characters
+
   return (
-    <div>
-      
-    </div>
-  )
+    <Carousel
+      plugins={[
+        Autoplay({
+          delay: 2000,
+        }),
+      ]}
+      opts={{
+        align: "start",
+      }}
+      className="w-full"
+    >
+      <CarouselContent>
+      {characters.map((char, index) => (
+          <CarouselItem key={index} className="basis-1/4 md:basis-1/5 lg:basis-1/6">
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center pb-0">
+                  <span className="text-3xl md:text-7xl text-white font-semibold">{char}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+    </Carousel>
+  );
 }
