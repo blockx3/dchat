7e1b5505bf1f5239f3f9d883589deb6cc1839064
Thinley <thinleylama44@gmail.com>
2024-10-19 23:02:10 +0530

    Test workflow

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -4,9 +4,7 @@ import Home from "./components/Home";
 export default function Page() {
   return (
     <div className="bg-[#0E0A24] min-h-screen">
-      <Home />
-      
-      
+      <Home />      
     </div>
   );
 }
