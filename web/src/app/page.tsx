31af7e936b2311dd1f1a57111a0019c64ff35dc9
Thinley <thinleylama44@gmail.com>
2024-10-19 22:14:12 +0530

    FEAT: Added workflow and Dockerfile

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,11 +1,9 @@
 import Home from "./components/Home";
 
-
 export default function Page() {
   return (
     <div className="bg-[#0E0A24] min-h-screen">
       <Home />
-      
     </div>
   );
 }
